7e35d69a129a81532a6c250ef7b69bad6e5a099f
tito.ardha <tito.ardha@windowslive.com>
2025-01-02 12:29:19 +0000

    change splitter char

--- pages/Scan QR Code/jsobjects/js_sentcode/js_sentcode.js
+++ pages/Scan QR Code/jsobjects/js_sentcode/js_sentcode.js
@@ -1,7 +1,7 @@
 export default {
 	scanner_QRCodeonCodeDetected () {
 		const VText = scanner_QRCode.value;
-		const AText = VText.split("/");
+		const AText = VText.split("\\");
 		txt_RegID.setText(AText[0]);
 		txt_MIAPCODE.setText(AText[1]);
 		txt_DCDCODE.setText(AText[2]);
