b73868e12814533055fd39a9d9d1ede0aac7188b
tito.ardha <tito.ardha@windowslive.com>
2025-01-02 11:54:02 +0000

    test function split string array in qrcode page

--- pages/Scan QR Code/jsobjects/js_sentcode/js_sentcode.js
+++ pages/Scan QR Code/jsobjects/js_sentcode/js_sentcode.js
@@ -3,5 +3,7 @@ export default {
 		const VText = scanner_QRCode.value;
 		const AText = VText.split("/");
 		txt_RegID.setText(AText[0]);
+		txt_MIAPCODE.setText(AText[1]);
+		txt_DCDCODE.setText(AText[2]);
 	}
 }
