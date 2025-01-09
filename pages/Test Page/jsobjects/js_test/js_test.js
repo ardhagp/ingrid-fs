de76a214ebe25bb764479103bf534f1874c5144a
tito.ardha <tito.ardha@windowslive.com>
2025-01-09 15:59:20 +0000

    add generate random string function

--- pages/Test Page/jsobjects/js_test/js_test.js
+++ pages/Test Page/jsobjects/js_test/js_test.js
@@ -4,4 +4,15 @@ export default {
     md.update(Input);
     return md.digest().toHex();
   },
+	makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
 };
