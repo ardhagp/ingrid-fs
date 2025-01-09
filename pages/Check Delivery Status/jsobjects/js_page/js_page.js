bf68bc80f0bb2b8196d19e6cf15c68c93147954f
tito.ardha <tito.ardha@windowslive.com>
2025-01-09 15:34:23 +0000

    add session to page

--- pages/Check Delivery Status/jsobjects/js_page/js_page.js
+++ pages/Check Delivery Status/jsobjects/js_page/js_page.js
@@ -1,13 +1,13 @@
 export default {
 autorun(){
 	try{
-		if (appsmith.store.UserID=='') {
-			navigateTo('Login or Register');
+		if (appsmith.store.Employee.IsLogged==false) {
+			navigateTo('Authorization');
 			console.log(appsmith.store.UserID);
 			return appsmith.store.UserID;
 		} else {			
-			console.log(appsmith.store.UserID);
-			return appsmith.store.UserID;
+			console.log(appsmith.store.Employee.UserID);
+			return appsmith.store.Employee.UserID;
 		}
    }catch(error){
 		console.log(error.message)
