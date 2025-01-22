d66d48bbd8005baee34b160d3167d825580affc4
tito.ardha <tito.ardha@windowslive.com>
2025-01-22 04:54:54 +0000

    fixing session

--- pages/Login & Register/jsobjects/js_page/js_page.js
+++ pages/Login & Register/jsobjects/js_page/js_page.js
@@ -6,7 +6,7 @@ export default {
 	},
 
 	async f_login() {
-		{{storeValue("Employee",{"UserID": txt_UserID.text, "UserPassword": js_page.md5(txt_Password.text)})}}
+		await storeValue("Employee",{"UserID": txt_UserID.text, "UserPassword": js_page.md5(txt_Password.text)});
 
 		await q_login.run();
 
@@ -14,14 +14,23 @@ export default {
 			txt_Result.setVisibility(false);
 			txt_Result.setText('Berhasil');
 			//console.log(q_login.data.length);
-			storeValue("Employee",{"IsLoggedIn": true, "IsLoggedOut": false,"UserID": txt_UserID.text,"IsAdmin": q_login.data[0].employee_isadmin,"IsLocked":q_login.data[0].employee_islocked,"FullName":q_login.data[0].employee_fullname,"ID":q_login.data[0].employee_id,"Role":q_login.data[0].employee_role,"Title":q_login.data[0].employee_title,"SLOC":q_login.data[0].employee_slocid});
+			let boolean_IsAdmin;
+			if	(q_login.data[0].employee_isadmin_boolean=='true') {
+				boolean_IsAdmin = true;
+			} else {
+				boolean_IsAdmin = false;
+			}
+			storeValue("Employee",{"IsLoggedIn": true, "IsLoggedOut": false,"UserID": txt_UserID.text,"IsAdmin": q_login.data[0].employee_isadmin,"IsLocked":q_login.data[0].employee_islocked,"FullName":q_login.data[0].employee_fullname,"ID":q_login.data[0].employee_id,"Role":q_login.data[0].employee_role,"Title":q_login.data[0].employee_title,"SLOC":q_login.data[0].employee_slocid,"IsAdminBoolean":boolean_IsAdmin});
+
 			storeValue("AppSettings",{"Authorization_Tab_Login_IsVisible": false,"Authorization_Tab_Register_IsVisible": false,"Authorization_Tab_Logout_IsVisible": true,"Authorization_Tab_Restricted_IsVisible": false,"Authorization_Tab_Default":"Logout"});
 			navigateTo('Login Success');
 		} else if (q_login.data.length===0) {
 			txt_Result.setVisibility(true);
 			txt_Result.setText('Login Gagal!');
 			//console.log(q_login.data.length);
-			await storeValue("Employee",{"IsLoggedIn": false, "IsLoggedOut": true});
+
+			await storeValue("Employee",{"IsLoggedIn": false, "IsLoggedOut": true,"IsAdminBoolean":false});
+
 			await storeValue("AppSettings",{"Authorization_Tab_Login_IsVisible": true,"Authorization_Tab_Register_IsVisible": true,"Authorization_Tab_Logout_IsVisible": false,"Authorization_Tab_Restricted_IsVisible": false,"Authorization_Tab_Default":"Login"});
 		}		
 	},
@@ -32,7 +41,9 @@ export default {
 		navigateTo('Home');
 	},
 
-	f_autorun() {
+	autorun() {
+		console.log(appsmith.store.Employee.IsLoggedIn);
+		console.log(appsmith.store.Employee.UserID);
 		if (appsmith.store.Employee.IsLoggedIn==true && appsmith.store.Employee.UserID != "") {
 			navigateTo('Logout');
 		}
