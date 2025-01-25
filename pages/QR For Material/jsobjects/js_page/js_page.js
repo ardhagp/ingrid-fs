1dd98f80515693e16e6e900660264c9f7bb8c8ea
tito.ardha <tito.ardha@windowslive.com>
2025-01-25 23:51:00 +0000

    update qr for material

--- pages/QR For Material/jsobjects/js_page/js_page.js
+++ pages/QR For Material/jsobjects/js_page/js_page.js
@@ -4,14 +4,14 @@ export default {
 		//let QRText = "493NAW0000196S\\A-2022-00004#0030\\2\\DCD\\GEN\\IMB1\\1MB1\\-\\-"
 		const QRText = await scanner_QRCode.value;
 		const SplittedText = await QRText.split("\\");
-		lbl_MIAPCODE.setText("MIAP CODE : <p style=color:red>" + SplittedText[0] + "</p>");
-		lbl_DCDCODE.setText("DCD CODE : <p style=color:red>" + SplittedText[1] + "</p>");
-		lbl_val_mattype.setText("VT / MT / MC: <p style=color:red>" + SplittedText[2] + " / " + SplittedText[3] + " / " + SplittedText[4] + "</p>");
-		lbl_PLAN_SLOC.setText("PLANT / SLOC : <p style=color:red>" + SplittedText[5] + " / " + SplittedText[6] + "</p>");
-		lbl_RegID.setText("REG. ID : <p style=color:red>" + SplittedText[7] + "</p>");
-		lbl_PublishCode.setText("PUBLISH : <p style=color:red>" + SplittedText[8] + "</P>");
+		await lbl_MIAPCODE.setText("MIAP CODE : <p style=color:red>" + SplittedText[0] + "</p>");
+		await lbl_DCDCODE.setText("DCD CODE : <p style=color:red>" + SplittedText[1] + "</p>");
+		await lbl_val_mattype.setText("VT / MT / MC: <p style=color:red>" + SplittedText[2] + " / " + SplittedText[3] + " / " + SplittedText[4] + "</p>");
+		await lbl_PLAN_SLOC.setText("PLANT / SLOC : <p style=color:red>" + SplittedText[5] + " / " + SplittedText[6] + "</p>");
+		await lbl_RegID.setText("REG. ID : <p style=color:red>" + SplittedText[7] + "</p>");
+		await lbl_PublishCode.setText("PUBLISH : <p style=color:red>" + SplittedText[8] + "</P>");
 		await storeValue("QR_Material",{"MIAP":SplittedText[0],"DCD":SplittedText[1],"VT":SplittedText[2],"MT":SplittedText[3],"MC":SplittedText[4],"PLANT":SplittedText[5],"SLOC":SplittedText[6],"REGID":SplittedText[7],"PUBLISH":SplittedText[8]});
-		txt_QRResult.setValue(QRText);
+		await txt_QRResult.setValue(QRText + " : " + SplittedText[0]);
 	},
 
 	async autorun() {
