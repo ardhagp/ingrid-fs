a173092a578cae8a54c0ec6156e004af29d28f7f
tito.ardha <tito.ardha@windowslive.com>
2025-01-25 23:58:21 +0000

    update qr for material

--- pages/QR For Material/jsobjects/js_page/js_page.js
+++ pages/QR For Material/jsobjects/js_page/js_page.js
@@ -5,6 +5,7 @@ export default {
 			//let QRText = "493NAW0000196S\\A-2022-00004#0030\\2\\DCD\\GEN\\IMB1\\1MB1\\-\\-"
 			const QRText = await scanner_QRCode.value;
 			const SplittedText = await QRText.split("\\");
+			await lbl_QRResult.setText("Result : " + QRText + "");
 			await lbl_MIAPCODE.setText("MIAP CODE : <p style=color:red>" + SplittedText[0] + "</p>");
 			await lbl_DCDCODE.setText("DCD CODE : <p style=color:red>" + SplittedText[1] + "</p>");
 			await lbl_val_mattype.setText("VT / MT / MC: <p style=color:red>" + SplittedText[2] + " / " + SplittedText[3] + " / " + SplittedText[4] + "</p>");
@@ -12,9 +13,8 @@ export default {
 			await lbl_RegID.setText("REG. ID : <p style=color:red>" + SplittedText[7] + "</p>");
 			await lbl_PublishCode.setText("PUBLISH : <p style=color:red>" + SplittedText[8] + "</P>");
 			await storeValue("QR_Material",{"MIAP":SplittedText[0],"DCD":SplittedText[1],"VT":SplittedText[2],"MT":SplittedText[3],"MC":SplittedText[4],"PLANT":SplittedText[5],"SLOC":SplittedText[6],"REGID":SplittedText[7],"PUBLISH":SplittedText[8]});
-			await txt_QRResult.setValue(QRText + " : " + SplittedText[0]);
 		}catch(error){
-			txt_QRResult.setValue(error);
+			console.log(error);
 		}
 	},
 
