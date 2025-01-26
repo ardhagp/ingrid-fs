fcfe7b2b91b138c69c15e396317d7c5e4f7b7239
tito.ardha <tito.ardha@windowslive.com>
2025-01-26 00:35:08 +0000

    update qr for material

--- pages/QR For Material/jsobjects/js_page/js_page.js
+++ pages/QR For Material/jsobjects/js_page/js_page.js
@@ -1,28 +1,37 @@
 export default {
 	f_manual(){
-			lbl_QRResult.setText("QR Result : " + appsmith.store.QR_Material.QRResult + "");
+		lbl_QRResult.setText("QR Result : " + appsmith.store.QR_Material.QRResult + "");
 	},
-	scanner_QRCodeonCodeDetected () {
+	f_ScanCode () {
 		try{
 			//QR Code format = "MIAPCODE\\DCDCODE\\VALTYPE\\MATERIALTYPE\\PLANT\\SLOC\\REGID\\PUBLISHCODE"
 			//let QRText = "493NAW0000196S\\A-2022-00004#0030\\2\\DCD\\GEN\\IMB1\\1MB1\\-\\-"
 			const QRText = scanner_QRCode.value;
-			const SplittedText = QRText.split("\\");
 			storeValue("QR_Material",{"QRResult":QRText});
-			lbl_QRResult.setText("QR Result : " + QRText + "");
-			lbl_MIAPCODE.setText("MIAP CODE : <p style=color:red>" + SplittedText[0] + "</p>");
-			lbl_DCDCODE.setText("DCD CODE : <p style=color:red>" + SplittedText[1] + "</p>");
-			lbl_val_mattype.setText("VT / MT / MC: <p style=color:red>" + SplittedText[2] + " / " + SplittedText[3] + " / " + SplittedText[4] + "</p>");
-			lbl_PLAN_SLOC.setText("PLANT / SLOC : <p style=color:red>" + SplittedText[5] + " / " + SplittedText[6] + "</p>");
-			lbl_RegID.setText("REG. ID : <p style=color:red>" + SplittedText[7] + "</p>");
-			lbl_PublishCode.setText("PUBLISH : <p style=color:red>" + SplittedText[8] + "</P>");
-			storeValue("QR_Material",{"MIAP":SplittedText[0],"DCD":SplittedText[1],"VT":SplittedText[2],"MT":SplittedText[3],"MC":SplittedText[4],"PLANT":SplittedText[5],"SLOC":SplittedText[6],"REGID":SplittedText[7],"PUBLISH":SplittedText[8]});
-			lbl_QRResult.setText("QRResult : " + QRText);
+			
 		}catch(error){
 			console.log(error);
 		}
 	},
-
+	async f_ExtractString(){
+		try{
+			const QRResultString = await appsmith.store.QR_Material.QRResult;
+			lbl_QRResult.setText("QRResult : " + QRResultString);
+			
+			const SplittedText = QRResultString.split("\\");
+			
+			await lbl_QRResult.setText("QR Result : " + QRResultString + "");
+			await lbl_MIAPCODE.setText("MIAP CODE : <p style=color:red>" + SplittedText[0] + "</p>");
+			await lbl_DCDCODE.setText("DCD CODE : <p style=color:red>" + SplittedText[1] + "</p>");
+			await lbl_val_mattype.setText("VT / MT / MC: <p style=color:red>" + SplittedText[2] + " / " + SplittedText[3] + " / " + SplittedText[4] + "</p>");
+			await lbl_PLAN_SLOC.setText("PLANT / SLOC : <p style=color:red>" + SplittedText[5] + " / " + SplittedText[6] + "</p>");
+			await lbl_RegID.setText("REG. ID : <p style=color:red>" + SplittedText[7] + "</p>");
+			await lbl_PublishCode.setText("PUBLISH : <p style=color:red>" + SplittedText[8] + "</P>");
+			storeValue("QR_Material",{"MIAP":SplittedText[0],"DCD":SplittedText[1],"VT":SplittedText[2],"MT":SplittedText[3],"MC":SplittedText[4],"PLANT":SplittedText[5],"SLOC":SplittedText[6],"REGID":SplittedText[7],"PUBLISH":SplittedText[8]});
+		}catch(error){
+			console.log(error);
+		}
+	},
 	async autorun() {
 		await storeValue("QR_Material",{"MIAP":"","DCD":"","VT":"","MT":"","MC":"","PLANT":"","SLOC":"","REGID":"","PUBLISH":""});
 	}
