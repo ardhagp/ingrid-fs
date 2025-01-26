7c8cb5e2afaa722ed27d34ad5cbcf635aab20b73
tito.ardha <tito.ardha@windowslive.com>
2025-01-26 01:05:39 +0000

    update qr for material

--- pages/QR For Material/jsobjects/js_page/js_page.js
+++ pages/QR For Material/jsobjects/js_page/js_page.js
@@ -22,18 +22,18 @@ export default {
 			const SplittedText = QRResultString.split("\\");
 			await storeValue("QR_Material",{"MIAP":SplittedText[0],"DCD":SplittedText[1],"VT":SplittedText[2],"MT":SplittedText[3],"MC":SplittedText[4],"PLANT":SplittedText[5],"SLOC":SplittedText[6],"REGID":SplittedText[7],"PUBLISH":SplittedText[8]});
 			
-			await lbl_QRResult.setText("QR Result : " + QRResultString + "");
-			await lbl_MIAPCODE.setText("MIAP CODE : <p style=color:red>" + appsmith.store.QR_Material.MIAP + "</p>");
-			await lbl_DCDCODE.setText("DCD CODE : <p style=color:red>" + appsmith.store.QR_Material.DCD + "</p>");
-			await lbl_val_mattype.setText("VT / MT / MC: <p style=color:red>" + appsmith.store.QR_Material.VT + " / " + appsmith.store.QR_Material.MT + " / " + appsmith.store.QR_Material.MC + "</p>");
-			await lbl_PLAN_SLOC.setText("PLANT / SLOC : <p style=color:red>" + appsmith.store.QR_Material.PLANT + " / " + appsmith.store.QR_Material.SLOC + "</p>");
-			await lbl_RegID.setText("REG. ID : <p style=color:red>" + appsmith.store.QR_Material.REGID + "</p>");
-			await lbl_PublishCode.setText("PUBLISH : <p style=color:red>" + appsmith.store.QR_Material.PUBLISH + "</P>");
+			//await lbl_QRResult.setText("QR Result : " + QRResultString + "");
+			//await lbl_MIAPCODE.setText("MIAP CODE : <p style=color:red>" + appsmith.store.QR_Material.MIAP + "</p>");
+			//await lbl_DCDCODE.setText("DCD CODE : <p style=color:red>" + appsmith.store.QR_Material.DCD + "</p>");
+			//await lbl_val_mattype.setText("VT / MT / MC: <p style=color:red>" + appsmith.store.QR_Material.VT + " / " + appsmith.store.QR_Material.MT + " / " + appsmith.store.QR_Material.MC + "</p>");
+			//await lbl_PLAN_SLOC.setText("PLANT / SLOC : <p style=color:red>" + appsmith.store.QR_Material.PLANT + " / " + appsmith.store.QR_Material.SLOC + "</p>");
+			//await lbl_RegID.setText("REG. ID : <p style=color:red>" + appsmith.store.QR_Material.REGID + "</p>");
+			//await lbl_PublishCode.setText("PUBLISH : <p style=color:red>" + appsmith.store.QR_Material.PUBLISH + "</P>");
 		}catch(error){
 			console.log(error);
 		}
 	},
 	async autorun() {
-		await storeValue("QR_Material",{"MIAP":"","DCD":"","VT":"","MT":"","MC":"","PLANT":"","SLOC":"","REGID":"","PUBLISH":""});
+		await storeValue("QR_Material",{"MIAP":"-","DCD":"-","VT":"-","MT":"-","MC":"-","PLANT":"-","SLOC":"-","REGID":"-","PUBLISH":"-"});
 	}
 }
