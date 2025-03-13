26ede15a6449ab6412b3f1d07eebe1a79c47eccc
tito.ardha <tito.ardha@windowslive.com>
2025-03-13 21:56:10 +0000

    update login flow

--- pages/Home/jsobjects/js_page/js_page.js
+++ pages/Home/jsobjects/js_page/js_page.js
@@ -5,13 +5,13 @@ export default {
 			storeValue("AppSettings",{"Page_SummaryNCR_DashboardContractorYear": "2025"});
 
 			if (!varIsLoggedIn||varIsLoggedIn===false){
-				storeValue("Employee",{"IsLoggedIn":false,"IsLoggedOut":true});
+				storeValue("Employee",{"IsLoggedIn":false,"IsLoggedOut":true,"UserID ":""});
 			} else {
 				storeValue("Employee",{"IsLoggedIn":true,"IsLoggedOut":false});
 			}
 		}catch(error){
 			console.log('Error: ' + error.message);
-			storeValue("Employee",{"IsLoggedIn":false,"IsLoggedOut":true,"IsFirstViewAfterLogin":false});
+			storeValue("Employee",{"IsLoggedIn":false,"IsLoggedOut":true,"IsFirstViewAfterLogin":false,"UserID ":""});
 		}
 	}
 }
