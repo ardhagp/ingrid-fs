02530d3886c57d780e594c1fb87b0c099b227644
Ardha Gp <tito.ardha@windowslive.com>
2025-03-14 04:53:56 +0700

    Update js_page.js
    critical update to login session

--- pages/Login Success/jsobjects/js_page/js_page.js
+++ pages/Login Success/jsobjects/js_page/js_page.js
@@ -1,6 +1,6 @@
 export default {
 	async autorun(){
-		if (appsmith.store.Employee.IsLoggedIn===false || appsmith.store.Employee.UserID != "") {
+		if (appsmith.store.Employee.IsLoggedIn===false || appsmith.store.Employee.UserID === "") {
 			navigateTo('Login & Register',{},'SAME_WINDOW');
 		}			
 	},
