18bc0688454b177b459a709fb7f684d3b74bca9c
tito.ardha <tito.ardha@windowslive.com>
2025-04-17 01:33:03 +0000

    update flow to open module

--- pages/Home/jsobjects/js_button/js_button.js
+++ pages/Home/jsobjects/js_button/js_button.js
@@ -14,10 +14,28 @@ export default {
 	async f_btn_SummaryNCR(){
 		await storeValue("AppSettings_LastPage","Summary NCR");
 
+		// check if user is logged in
 		if (appsmith.store.Employee_IsLoggedIn===true){
-			await storeValue("AppSettings_Module_Open","APP_MAT_SUM_NCR");
-			await navigateTo(appsmith.store.AppSettings_LastPage,{},'SAME_WINDOW');
-		}else{
+
+			// check if user has access to modules if he is not an admin
+			if (appsmith.store.Employee_IsAdmin===false){
+				await q_access_level.run();
+			}
+
+			if (appsmith.store.Employee_IsAdmin===true){
+				// open module if admin
+				await storeValue("AppSettings_Module_Open","APP_MAT_SUM_NCR");
+				await navigateTo(appsmith.store.AppSettings_LastPage,{},'SAME_WINDOW');
+			} else if (appsmith.store.Employee_IsAdmin===false && q_access_level.data[0].is_exist===1) {
+				// open module if not admin and have access
+				await storeValue("AppSettings_Module_Open","APP_MAT_SUM_NCR");
+				await navigateTo(appsmith.store.AppSettings_LastPage,{},'SAME_WINDOW');
+			} else {
+				// redirect to restricted page if have no access
+				await navigateTo('Restricted',{},'SAME_WINDOW');
+			}			
+		} else {
+			// redirect to login page if not logged in
 			await navigateTo('Login & Register',{},'SAME_WINDOW');
 		}
 	},
@@ -48,11 +66,14 @@ export default {
 
 	async f_btn_ClearVariables(){
 		clearStore();
+		// variables for confirmation
 		await storeValue("Employee_IsLoggedIn",false);
 		await storeValue("Employee_IsLoggedOut",true);
-		await storeValue("Employee_UserID","");
 		await storeValue("Employee_IsAdmin",false);
-		await storeValue("Employee_IsAdminBoolean",false);
 		await storeValue("Employee_IsFirstViewAfterLogin",false);
+
+		// variables for forms
+		await storeValue("Employee_ID","");
+		await storeValue("Employee_UserID","");
 	}
 }
